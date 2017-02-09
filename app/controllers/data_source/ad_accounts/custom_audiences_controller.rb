927675e435f381682ffccbc87eb0af6dc34fc635
parasquid <tristan.gomez@gmail.com>
2017-02-09 14:53:02 +0800

    get information from fb about the custom audiences
    we'll first show these 5 fields since they seem to be the most useful
    in figuring out whether the custom audience integration is actually
    working or not

--- app/controllers/data_source/ad_accounts/custom_audiences_controller.rb
+++ app/controllers/data_source/ad_accounts/custom_audiences_controller.rb
@@ -1,5 +1,21 @@
 class DataSource::AdAccounts::CustomAudiencesController < ApplicationController
   layout false
+
   def index
+    token = current_user.token
+    graph = Koala::Facebook::API.new(token)
+    custom_audiences = graph.get_connections("act_226287978", "customaudiences",
+      fields: [:name, :description, :approximate_count, :operation_status, :delivery_status]
+    )
+    # binding.pry
+    @results = custom_audiences.map { |data|
+      {
+        name: data["name"],
+        description: data["description"],
+        approximate_count: data["approximate_count"],
+        operation_status: data["operation_status"]["description"],
+        delivery_status: data["delivery_status"]["description"]
+      }
+    }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :authentications
+
+  def most_recent_authentication
+    authentications.order(updated_at: :desc).first
+  end
+
+  def token
+    most_recent_authentication.token
+  end
 end
