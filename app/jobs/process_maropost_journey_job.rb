673fea49864fb9031c91773e56e6f8749a2052ec
parasquid <tristan.gomez@gmail.com>
2017-02-13 14:37:32 +0800

    push the email into all custom audiences that is linked to this workflow
    need to think about api rate limits at this point ...

--- app/jobs/process_maropost_journey_job.rb
+++ app/jobs/process_maropost_journey_job.rb
@@ -1,20 +1,24 @@
 class ProcessMaropostJourneyJob < ApplicationJob
   def perform(params)
     workflow_id = params["workflow_id"]
-    custom_audience = Workflow.where(mp_id: workflow_id).first.custom_audience
-    custom_audience_fb_id = custom_audience.fb_id
-    email = params["contact"]["email"]
-    payload = {
-      schema: "EMAIL_SHA256",
-      data: [Digest::SHA256.hexdigest(email)]
-    }
+    workflows = Workflow.where(mp_id: workflow_id)
 
-    current_user = custom_audience.user
-    token = current_user.token
-    graph = Koala::Facebook::API.new(token)
+    workflows.each do |workflow|
+      custom_audience = workflow.custom_audience
+      custom_audience_fb_id = custom_audience.fb_id
+      email = params["contact"]["email"]
+      payload = {
+        schema: "EMAIL_SHA256",
+        data: [Digest::SHA256.hexdigest(email)]
+      }
 
-    result = graph.put_connections(custom_audience_fb_id, "users", payload:payload.to_json)
-    LOGGER.info result
+      current_user = custom_audience.user
+      token = current_user.token
+      graph = Koala::Facebook::API.new(token)
+
+      result = graph.put_connections(custom_audience_fb_id, "users", payload:payload.to_json)
+      LOGGER.info result
+    end
   end
 
 end
